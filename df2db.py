dd54721310ff1a1638009e936931a5bd35e6adce
chu3401 <chu3401@gmail.com>
2025-12-26 15:56:39 +0800

    add save to MSSQL

--- df2db.py
+++ df2db.py
@@ -6,37 +6,80 @@ Created on Wed Dec 24 16:22:04 2025
 """
 
 import mysql.connector
+import pyodbc
 import pandas as pd
 
-class data2mysql:
-    def __init__(self,df):
+class DataSave2SQL:
+    def __init__(self, df:pd.DataFrame): #type hint
         self.df = df
     
-    def save2sql(self):
+    def save(self):
+        raise NotImplementedError("save") #?
+
+class DataSave2MySQL(DataSave2SQL):
+    def __init__(self, df, host='localhost', user='root', password='password', database='db'):
+        super().__init__(df)
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+    
+    def save(self):
         conn = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='password',
-            database='db',
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database,
             charset='utf8mb4'
         )
-        
         cursor = conn.cursor()
-
         columns = self.df.columns.tolist()
         sql = f"""
         INSERT INTO news ({', '.join(columns)})
-        values ({", ".join(["%s"]*len(columns))})
+        VALUES ({', '.join(['%s']*len(columns))})
         """
-        
-        for i, row in self.df.iterrows():
+        for _, row in self.df.iterrows():
             values = tuple(row[col] for col in columns)
             cursor.execute(sql, values)
-            
         conn.commit()
+        conn.close()
+    
+    def preprocessing(self):
+        self.df = self.df.where(pd.notnull(self.df), None)
+        
+        
+    
+    
+
+class DataSave2MSSQL(DataSave2SQL):
+    def __init__(self, df, server='localhost', database='db', user='sa', password='password'):
+        super().__init__(df)
+        self.server = server
+        self.database = database
+        self.user = user
+        self.password = password
+
+    def save(self):
+        conn_str = (
+            f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+            f'SERVER={self.server};DATABASE={self.database};UID={self.user};PWD={self.password}'
+        )
+        conn = pyodbc.connect(conn_str)
+        cursor = conn.cursor()
+        columns = self.df.columns.tolist()
+        sql = f"""
+        INSERT INTO news ({', '.join(columns)})
+        VALUES ({', '.join(['?']*len(columns))})
+        """
+        for _, row in self.df.iterrows():
+            values = tuple(row[col] for col in columns)
+            cursor.execute(sql, values)
+        conn.commit()
+        conn.close()
 
 if __name__ == "__main__":    
     df=pd.read_csv('yourcsv.csv')
-    data2mysql(df)
-    data2mysql.save2sql()
-
+    
+    saver:DataSave2SQL = DataSave2MySQL(df)
+    #saver:DataSave2SQL = DataSave2MySQL(df)
+    saver.save()
